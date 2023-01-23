419c2b0fcba03e48127025824dc3e41aa98d7aef
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-01-23 14:50:35 -0300

    Update update_sta_lta.sql

--- Work Projects/update_sta_lta.sql
+++ Work Projects/update_sta_lta.sql
@@ -8,7 +8,7 @@ DECLARE @CadManagement varchar(MAX)
 SET @CadManagement = CONCAT(' DECLARE @management_key as int
 					SET @management_key = (SELECT management_key FROM fact_lake WHERE id =', :id, ') ',
 					' UPDATE dim_management
-					SET user_input_STALTA =',				CHAR(39), :user, CHAR(39),
+					SET user_input_STALTA =',				CHAR(39), user, CHAR(39),
 					' WHERE id = @management_key')
 
 -- Quando o usuário estiver editando um STA/LTA existente, deve ser salvo na dim_management o usuário de quem edita, assim como a adata da edição
@@ -17,7 +17,7 @@ SET @EditManagement = CONCAT(' DECLARE @management_key as int
 					SET @management_key = (SELECT management_key FROM fact_lake WHERE id =', :id, ') ',
 					' UPDATE dim_management
 					SET last_edit_date = ',					CHAR(39), GETDATE(), CHAR(39), 
-					', last_edit_user = ',					CHAR(39), :user, CHAR(39),
+					', last_edit_user = ',					CHAR(39), user, CHAR(39),
 					' WHERE id = @management_key')
 
 
@@ -26,18 +26,18 @@ SET @EditManagement = CONCAT(' DECLARE @management_key as int
 DECLARE @UpdateSTA varchar(MAX)
 SET @UpdateSTA = CONCAT('DECLARE @NPR_key as int
 					SET @NPR_key = (SELECT NPR_key FROM fact_lake WHERE id =', :id, ')',
-					' UPDATE dim_NPR SET STA_severity =', 	:severity,
-					', STA_occurrence =', 					:occurrence,
-					', STA_detection =', 					:detection,
-					', NPR_STA =', 							:NPR,
+					' UPDATE dim_NPR SET STA_severity =', 			severity,
+					', STA_occurrence =', 					occurrence,
+					', STA_detection =', 					detection,
+					', NPR_STA =', 						NPR,
 					' WHERE id = @NPR_key',
 					' DECLARE @term_action_key as int
 					SET @term_action_key = (SELECT term_action_key FROM fact_lake WHERE id =', :id, ') ',
 					' UPDATE dim_term_action
-					SET STA_description = ', 				CHAR(39), :description, CHAR(39),
-					', STA_responsable = ', 				CHAR(39), :responsable, CHAR(39),
-					', STA_deadline = ', 					CHAR(39), :deadline, CHAR(39),
-					', STA_status = ', 						:status,
+					SET STA_description = ', 				CHAR(39), description, CHAR(39),
+					', STA_responsable = ', 				CHAR(39), responsable, CHAR(39),
+					', STA_deadline = ', 					CHAR(39), deadline, CHAR(39),
+					', STA_status = ', 					status,
 					' WHERE id = @term_action_key')
 
 
@@ -46,18 +46,18 @@ DECLARE @UpdateLTA varchar(MAX)
 SET @UpdateLTA = CONCAT('DECLARE @NPR_key as int
 					SET @NPR_key = (SELECT NPR_key FROM fact_lake WHERE id =', :id, ')',
 					' UPDATE dim_NPR SET LTA_severity =',	:severity,
-					', LTA_occurrence =', 					:occurrence,
-					', LTA_detection =', 					:detection,
-					', NPR_LTA =', 							:NPR,
+					', LTA_occurrence =', 					occurrence,
+					', LTA_detection =', 					detection,
+					', NPR_LTA =', 						NPR,
 					' WHERE id = @NPR_key',
 					
 					' DECLARE @term_action_key as int
 					SET @term_action_key = (SELECT term_action_key FROM fact_lake WHERE id =', :id, ') ',
 					' UPDATE dim_term_action
-					SET LTA_description = ', 				CHAR(39), :description, CHAR(39),
-					', LTA_responsable = ', 				CHAR(39), :responsable, CHAR(39),
-					', LTA_deadline = ', 					CHAR(39), :deadline, CHAR(39),
-					', LTA_status = ',						:status,
+					SET LTA_description = ', 				CHAR(39), description, CHAR(39),
+					', LTA_responsable = ', 				CHAR(39), responsable, CHAR(39),
+					', LTA_deadline = ', 					CHAR(39), deadline, CHAR(39),
+					', LTA_status = ',					status,
 					' WHERE id = @term_action_key')
 
 
