fa54fa2be808c7987a3b0d29bbe403157ef1abad
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-01-23 14:44:29 -0300

    Create update_risk.sql

---
+++ Work Projects/update_risk.sql
@@ -0,0 +1,56 @@
+SET LANGUAGE Portuguese
+
+DECLARE @ID	AS INT 
+SET @ID = id
+
+--------------------------------------- NPR
+
+DECLARE @NPR_key as int 
+set @NPR_key = (SELECT NPR_key FROM [dbo].[fact_lake] WHERE id = @ID)
+
+UPDATE dim_NPR
+SET    NPR_severity = NPR_severity 
+      ,NPR_occurrence = NPR_occurrence 
+      ,NPR_detection = NPR_detection 
+      ,NPR = NPR 
+WHERE id = @NPR_key
+
+--------------------------------------- MANAGEMENT
+
+DECLARE @management_key as int 
+set @management_key = (SELECT management_key FROM [dbo].[fact_lake] WHERE id = @ID)
+
+UPDATE dim_management
+SET   last_edit_date = GETDATE()
+	    ,last_edit_user =  :user 
+WHERE id = @management_key
+
+------------------------------------------ MAP
+
+UPDATE fact_lake SET map_key =  :station_description 
+WHERE id =  @ID
+
+------------------------------------------ RISK
+
+DECLARE @risk_key AS INT 
+set @risk_key = (SELECT risk_key FROM [dbo].[fact_lake] WHERE id = @ID)
+
+DECLARE @component_code AS INT
+SET @component_code = (SELECT component_code FROM [dbo].[standard_component] WHERE id = :component_key)
+
+UPDATE dim_risk
+SET    document_type_key =  document_type_key 
+      ,document_number =  document_number 
+      ,component_key = component_key
+      ,component_code = @component_code
+      ,detail_key =  detail_key 
+      ,local_key =  local_key 
+      ,element_name =  element_name 
+      ,requeriment_key =  requeriment_key 
+      ,defect_key =  defect_key 
+      ,effect_key =  effect_key 
+      ,cause =  cause 
+      ,control_key =  control_key 
+      ,code =  code 
+      ,aud_cod_key =  aud_cod_key 
+WHERE id = @risk_key
